5afa75be9b5c4f661d83ce6cdaa1a92aaa165a15
DonutDonkey <34782950+DonutDonkey@users.noreply.github.com>
2019-12-04 14:14:53 +0100

    Create Main.java

---
+++ PolimorphizmThree/src/pl/accenture/Main.java
@@ -0,0 +1,31 @@
+package pl.accenture;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Vehicle [] vehicles = {new Vehicle(), new Ship(), new Plane(), new Car(), new Truck(), new RaceCar()};
+
+        for (Vehicle ve: vehicles ) {
+            if(ve instanceof RaceCar) {
+                ve.Base();
+                System.out.println(((RaceCar) ve).GetCarName() + System.lineSeparator() + ((RaceCar) ve).GetTurbo());
+            } else if (ve instanceof Truck) {
+                ve.Base();
+                System.out.println(((Truck) ve).GetCarName() + System.lineSeparator() + ((Truck) ve).GetFuel());
+            } else if (ve instanceof Car) {
+                ve.Base();
+                System.out.println(((Car) ve).GetCarName());
+            } else if (ve instanceof Plane) {
+                ve.Base();
+                System.out.println(((Plane) ve).GetPlaneName());
+            } else if (ve instanceof Ship) {
+                ve.Base();
+                System.out.println(((Ship) ve).GetShipName());
+            } else {
+                ve.Base();
+            }
+        }
+
+    }
+}
